7d05068e0db5b2b64b8c34f15b302dffa3d7efb6
majocmatt <majocmatt@gmail.com>
2023-09-09 11:39:31 -0400

    Delete menu.php

--- menu.php
+++
@@ -1,412 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Bonsai Sushi Menu</title>
-<meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<link rel="shortcut icon" href="image_main/favicon.png" type="image/x-icon" />
-<link type="text/css" rel="stylesheet" href="css/_main.css" />
-<!--[if lte IE 7]>
-	<script src="includes/pngfix.js"></script>
-<![endif]-->
-</head>
-
-<body id="home">
-
-<div id="container">
-
-<?php include("includes/header.php"); ?>
-
-<?php include("includes/nav.php"); ?>
-
-<div class="clearAll"></div>
-
-<div id="copy">
-
-<div id="rightColumnSub">
-<?php
-	if($_GET['type'] == "appetizers"){
-	?>
-	<h1>Soup and Salad</h1>
-	<p><h2>Miso Soup</h2></p>
-	<p><h2>Seafood Dobin (Tea Pot) Soup</h2>
-	<span>Shrimp, scallops, fish, fish cake and vegetables in a broth. Served in a lovely tea pot.</span></p>
-	<p><h2>House Salad</h2>
-	<span>Served with our special ginger dressing.</span></p>
-	<p><h2>Baby Octopus Salad</h2></p>
-	<p><h2>Seaweed Salad</h2></p>
-	<p><h2>Ika SanSai Salad</h2>
-	<span>Tenderized seasoned squid and mountain vegetables salad.</span></p>
-	<p><h2>Oshinko</h2></p>
-	
-	<h1>Cold Dish Appetizers</h1>
-	<p><h2>Bonsai Tree</h2>
-	<span>Smoked salmom, avocado, cream cheese, crab stick, masago, wrapped with paper thin cucumber served with ginger-tomato vinaigrette.</span></p>
-	<p><h2>Spicy Tuna Tataki</h2>
-	<span>9 spices crusted tuna lightly seared served with yuzu-ponzu sauce. Garnish with soy-balsamic mix spring salad.</span></p>
-	<p><h2>Halibut Usuzukuri</h2>
-	<span>Paper thin sliced halibut served with yuzu-ponzu sauce.</span></p>
-	<p><h2>Spicy Octopus with Cucumber Relish</h2>
-	<span>Sliced octopus marinated with chef's spicy sauce served with cucumber relish. Garnish with curry oil.</span></p>
-	<p><h2>Seafood Nuta</h2>
-	<span>Tuna, clam, conch, octopus mixed with su-miso.</span></p>
-	<p><h2>Tako Su</h2>
-	<span>Octopus, cucumber with sweet vinegar.</span></p>
-	<p><h2>Ebi Su</h2>
-	<span>Shrimp, cucumber with sweet vinegar.</span></p>
-	<p><h2>Mori Su</h2>
-	<span>Shrimp, octopus, clam, conch, crab stick, cucumber with sweet vinegar.</span></p>
-	<p><h2>Mango Scallop Ceviche</h2>
-	<span>Fresh scallop, mango, tomato, onion and cilantro mix with citrus sauce.</span></p>
-	<p><h2>Goma Ae</h2>
-	<span>Cold boiled spinach with sesame dressing.</span></p>
-	<p><h2>Sushi Appetizer</h2>
-	<span>1 piece tuna, 1 piece whitefish, 1 piece salmon, 1 piece shrimp.</span></p>
-	<p><h2>Sashimi Appetizer</h2>
-	<span>Assorted sliced raw fish.</span></p>
-
-	<h1>Hot Dish Appetizers</h1>
-	<p><h2>Scallop Isobe Maki</h2>
-	<span>Fresh scallop, asparagus, crab stick roll with seaweed then fried.</span></p>
-	<p><h2>BBQ Calamari</h2>
-	<span>Grilled calamari with ginger sauce.</span></p>
-	<p><h2>Green Mussels Motoyaki</h2>
-	<span>Baked half shell New Zealand green mussels with creamy sauce.</span></p>
-	<p><h2>Soft Shell Crab Tempura</h2>
-	<span>Deep fried soft shell crab with ponzu sauce.</span></p>
-	<p><h2>Tempura</h2>
-	<span>Deep fried shrimp and vegetables.</span></p>
-	<p><h2>Vegetable Tempura</h2>
-	<span>Deep fried vegetables.</span></p>
-	<p><h2>Ebi Gyoza</h2>
-	<span>Pan-fried shrimp dumpling served with soy-vinegar sauce.</span></p>
-	<p><h2>Kani Shu-Mai</h2>
-	<span>Steam crab dumplings.</span></p>
-	<p><h2>Yakitori</h2>
-	<span>Broiled chicken and scallion on skewer with special house teriyaki sauce.</span></p>
-	<p><h2>Hamachi Kama</h2>
-	<span>Broiled yellowtail jaw with teriyaki sauce.</span></p>
-	<p><h2>Negimaki</h2>
-	<span>Broiled beef rolled in scallion with teriyaki sauce.</span></p>
-	<p><h2>Age Dashi Tofu</h2>
-	<span>Deep fried tofu served with chef's special sauce.</span></p>
-	<p><h2>Eda Mame</h2>
-	<span>Steamed young soy bean.</span></p>
-	<?php
-	} elseif($_GET['type'] == "regularsushi") {
-	?>
-	<h1>Cooked Sushi Rolls</h1>
-	<p><h2>Bagel Roll</h2>
-	<span>Smoked salmon, avocado, cream cheese.</span></p>
-	<p><h2>Boston Roll</h2>
-	<span>Shrimp, cucumber, lettuce, mayo.</span></p>
-	<p><h2>California Roll</h2>
-	<span>Crab stick, avocado, cucumber, masago.</span></p>
-	<p><h2>Crazy Roll</h2>
-	<span>Eel, cucumber, salmon skin, green onion.</span></p>
-	<p><h2>Dancing Eel Roll</h2>
-	<span>California roll topped with eel.</span></p>
-	<p><h2>Dragon Roll</h2>
-	<span>Eel, cucumber, topped with avocado.</span></p>
-	<p><h2>Futo Maki</h2>
-	<span>Crab stick, cucumber, kampyo, egg, fish powder, spinach.</span></p>
-	<p><h2>Green River Roll</h2>
-	<span>Eel, cucumber, avocado, outside covered with green seaweed powder.</span></p>
-	<p><h2>Key West Roll</h2>
-	<span>Spicy conch, cucumber, topped with wasabi tobiko.</span></p>
-	<p><h2>Mexican Sunrise Roll</h2>
-	<span>Shrimp tempura, cream cheese, onion, jalapeno, tomato, lettuce, avocado, Bonsai aioli sauce.</span></p>
-	<p><h2>Miami Vice Roll</h2>
-	<span>White fish tempura, lettuce, avocado, spicy sauce.</span></p>
-	<p><h2>New York Roll</h2>
-	<span>Salmon skin, green onion, masago, kaiware.</span></p>
-	<p><h2>911 Roll</h2>
-	<span>Calimari tempura, cucumber, masago, spicy sauce.</span></p>
-	<p><h2>Rio Grande Roll</h2>
-	<span>Eel, shrimp, asparagus, cucumber, avocado.</span></p>
-	<p><h2>Rock n&rsquo; Roll</h2>
-	<span>Eel, scallop, avocado.</span></p>
-	<p><h2>Hard Rock n' Roll</h2>
-	<span>Eel, scallop, avocado, black caviar, miso garlic sauce.</span></p>
-	<p><h2>Shrimp Tempura Roll</h2>
-	<span>Shrimp tempura, lettuce, cucumber, masago, mayo.</span></p>
-	<p><h2>Spider Roll</h2>
-	<span>Soft shell crab, cucumber, masago.</span></p>
-	<p><h2>Super Crunch Roll</h2>
-	<span>Tempura crumbs with special sauce topped with shrimp.</span></p>
-	<p><h2>Tampa Roll</h2>
-	<span>Grouper tempura, cucumber, mayo.</span></p>
-	<p><h2>Unakyu Roll</h2>
-	<span>Eel, cucumber.</span></p>
-	<p><h2>Volcano Roll</h2>
-	<span>Crab stick, cucumber, avocado, cream cheese, masago, topped with spicy sauce.</span></p>
-	
-	<h1>Raw Sushi Rolls</h1>
-	<p><h2>Alaskan Roll</h2>
-	<span>Salmon, avocado, cucumber.</span></p>
-	<p><h2>Beauty and Beast</h2>
-	<span>Half eel and half cream cheese, the other half tun and avocado.</span></p>
-	<p><h2>Big Roni Roll</h2>
-	<span>Tuna, asparagus, avocado, masago, deep fried.</span></p>
-	<p><h2>Bonsai Special Roll </h2>
-	<span>Tuna, salmon, yellow tail, crab, asparagus, cucumber, kaiware, egg, yamagobo with Bonsai aioli sauce.</span></p>
-	<p><h2>Dynamite Roll</h2>
-	<span>Yellow tail, tuna, green onion, miso garlic sauce.</span></p>
-	<p><h2>Jung Roll</h2>
-	<span>Tuna, yellow tail, salmon, scallion, masago.</span></p>
-	<p><h2>Kamikaze Roll</h2>
-	<span>Spicy tuna, avocado, green onion, miso garlic sauce.</span></p>
-	<p><h2>Negihamachi Roll</h2>
-	<span>Yellow tail, green onion.</span></p>
-	<p><h2>Rainbow Roll</h2>
-	<span>California roll topped with tuna, salmon, white fish and avocado.</span></p>
-	<p><h2>Sapporo Roll</h2>
-	<span>Crab stick, asparagus, topped with fresh salmon, black caviar.</span></p>
-	<p><h2>Surf &amp; Turf Roll</h2>
-	<span>Tuna, cucumber, shiso leaf.</span></p>
-	<p><h2>Tuna Fantasy Roll</h2>
-	<span>Tuna roll topped with tuna.</span></p>
-
-	<h1>Veggie Sushi Rolls</h1>
-	<p><h2>Avocado Roll</h2></p>
-	<p><h2>Cucumber Roll</h2></p>
-	<p><h2>A &amp; A Roll</h2>
-	<span>Avocado, asparagus.</span></p>
-	<p><h2>Popeye Roll</h2>
-	<span>Spinach, yamagobo, miso garlic sauce.</span></p>
-	<p><h2>Vegetable Roll</h2>
-	<span>Kaiware, asparagus, cucumber, avocado, yamagobo.</span></p>
-	
-	<h1>Sushi Platters &amp; Combos</h1>
-	<p><h2>Regular Combo</h2>
-	<span>7 pieces of nigiri and a tuna roll.</span></p>
-	<p><h2>Deluxe Combo</h2>
-	<span>9 pieces of nigiri and a california roll.</span></p>
-	<p><h2>Supreme Combo</h2>
-	<span>12 pieces of nigiri and a california roll.</span></p>
-	<p><h2>Sashimi Plate</h2>
-	<span>Assorted slices of raw fish.</span></p>
-	<p><h2>Roll Combo</h2>
-	<span>1 tuna roll, 1 california roll, 1 boston roll.</span></p>
-	<p><h2>Tekka Don</h2>
-	<span>Raw tuna fillets on a bed of seasoned rice.</span></p>
-	<p><h2>Chirashi</h2>
-	<span>Assorted raw fillets ona bed of seasoned rice.</span></p>
-	<p><h2>Tuna Symphony</h2>
-	<span>Tuna sashimi, tuna nigiri sushi, tuna roll, tuna tataki.</span></p>
-	<p><h2>Love Boat for Two</h2>
-	<span>20 pieces and 2 rolls.</span></p>
-	
-	<?php
-	} elseif($_GET['type'] == "japanesedishes") {
-	?>
-	<h1>Japanese Dishes</h1>
-	<p><h2>Chicken Teriyaki</h2>
-	<span>Tepan grilled boneless chicken breast with teriyaki sauce.</span></p>
-	<p><h2>Beef Teriyaki</h2>
-	<span>Tepan grilled New York strip steak with teriyaki sauce.</span></p>
-	<p><h2>Shrimp teriyaki</h2>
-	<span>Sauteed shrimp and vegetable in teriyaki sauce.</span></p>
-	<p><h2>Scallop Teriyaki</h2>
-	<span>Sauteed scallop and vegetable in teriyaki sauce.</span></p>
-	<p><h2>Salmon Teriyaki</h2>
-	<span>Tepan grilled salmon with teriyaki sauce.</span></p>
-	<p><h2>Beef Negima</h2>
-	<span>Thin sliced beef roll with scallion in teriyaki sauce.</span></p>
-	<p><h2>Chicken Misoyaki</h2>
-	<span>Tepan grilled chicken with miso sauce.</span></p>
-	<p><h2>Beef Misoyaki</h2>
-	<span>Tepan grilled New York strip steak with miso sauce.</span></p>
-	<p><h2>Shrimp Misoyaki</h2>
-	<span>Sauteed shrimp and vegetables in miso sauce.</span></p>
-	<p><h2>Scallop Misoyaki</h2>
-	<span>Sauteed scallop with vegetables in miso sauce.</span></p>
-	<p><h2>Salmon Misoyaki</h2>
-	<span>Tepan grilled salmon with miso sauce.</span></p>
-	<p><h2>Tuna Steak Misoyaki</h2>
-	<span>Tepan grillied tuna with miso sauce.</span></p>
-	<p><h2>Tuna Steak</h2>
-	<span>Grilled sushi grade tuna with garlic pepper sauce.</span></p>
-	<p><h2>Yasai Itame</h2>
-	<span>Stir fry vegetables.</span></p>
-	<p><h2>Chicken Yasai Itame</h2>
-	<span>Stir fry chicken and vegetables.</span></p>
-	<p><h2>Shrimp Yasai Itame</h2>
-	<span>Stir fry shrimp and vegetables.</span></p>
-	<p><h2>Galbi</h2>
-	<span>Grilled marinated beef short ribs.</span></p>
-	<p><h2>Bul Go Gi</h2>
-	<span>Satay thin sliced marinated beef cooked with onion.</span></p>
-	<p><h2>Spicy BBQ Pork</h2>
-	<span>Satay thin sliced marinated pork cooked with onion.</span></p>
-	<p><h2>Shrimp Tempura</h2>
-	<span>Deep fried shrimp and vegetables.</span></p>
-	<p><h2>Vegetables Tempura</h2>
-	<span>Deep fried vegetables.</span></p>
-	<p><h2>Seafood Tempura Combo</h2>
-	<span>Deep fried shrimp, scallop, fish, crab stick and vegetables.</span></p>
-	<p><h2>Tonkatsu</h2>
-	<span>Fried breaded pork cutlet Japanese style.</span></p>
-	<p><h2>Chicken Katsu</h2>
-	<span>Fried breaded chicken cutlet Japanese style.</span></p>
-	
-	<h1>Rice &amp; Noodles</h1>
-	<p><h2>Tempura Udon</h2>
-	<span>Japanese wide noodle soup with shrimp and vegetables tempura.</span></p>
-	<p><h2>Nabeyaki Udon</h2>
-	<span>Japanese wide noodle soup with chicken, fish cake, egg, vegetables and shrimp tempura.</span></p>
-	<p><h2>Seafood Nabeyaki Udon</h2>
-	<span>Japanese wide noodle soup with shrimp, scallop, fish, egg, mussels and vegetables.</span></p>
-	<p><h2>Yaki Udon</h2>
-	<span>Pan-fried Japanese wide noodles with chicken and vegetables.</span></p>
-	<p><h2>Una Ju</h2>
-	<span>Charcoal-broiled eel in mildly sweet sauce on a bed of rice.</span></p>
-	
-	<h1>Combination Dinner</h1>
-	<p><h2>Chicken Teriyaki &amp; Shrimp Tempura</h2></p>
-	<p><h2>Beef Teriyaki &amp; Shrimp Tempura</h2></p>
-	<p><h2>Chicken Teriyaki &amp; Sushi</h2></p>
-	<p><h2>Beef Teriyaki &amp; Sushi</h2></p>
-	<p><h2>Galbi &amp; Shrimp Tempura</h2></p>
-	<p><h2>Yakiniku Bul Go Gi &amp; Tempura</h2></p>
-	
-	<h1>Vegetable Dishes</h1>
-	<p><h2>Vegetable Sushi &amp; Veggie Tempura</h2></p>
-	<p><h2>Yasai Itame - vege stir fry - &amp; Vege Tempura</h2></p>
-	
-	<?php
-	} elseif($_GET['type'] == "nigiri") {
-	?>
-	<h1>Raw Nigiri</h1>
-	<p><h2>Tuna (Maguro)</h2></p>
-	<p><h2>White Tuna</h2></p>
-	<p><h2>Salmon (Sake)</h2></p>
-	<p><h2>Yellowtail (Hamachi)</h2></p>
-	<p><h2>White Fish</h2></p>
-	<p><h2>Sweet Shrimp (Ama Ebi)</h2></p>
-	<p><h2>Squid (Ika)</h2></p>
-	<p><h2>Mackeral (Saba)</h2></p>
-	<p><h2>Conch</h2></p>
-	<p><h2>Sea Urchin (Uni)</h2></p>
-	<p><h2>Spicy tuna</h2></p>
-	<p><h2>Spicy Scallop(Spicy Hotate)</h2></p>
-	<p><h2>Semlt Roe (Masago)</h2></p>
-	<p><h2>Flying Fish Egg (Wasabi Tobiko)</h2></p>
-	<p><h2>Salmon Roe (Ikura)</h2></p>
-	<p><h2>Quail Egg</h2></p>
-	
-	<h1>Cooked Nigiri</h1>
-	<p><h2>Crab Stick (Kani)</h2></p>
-	<p><h2>Crab Delight</h2></p>
-	<p><h2>Smoked Salmon</h2></p>
-	<p><h2>Octopus (Tako)</h2></p>
-	<p><h2>Shrimp (Ebi)</h2></p>
-	<p><h2>Smoked Eel (Unagi)</h2></p>
-	<p><h2>Surf Clam (Hokkigai)</h2></p>
-	<p><h2>Tofu Skin (Inari)</h2></p>
-	<p><h2>Egg (Tamago)</h2></p>
-	
-	<h1>Sashimi</h1>
-	<p><h2>Tuna (Maguro)</h2></p>
-	<p><h2>White Tuna</h2></p>
-	<p><h2>Salmon (Sake)</h2></p>
-	<p><h2>Yellowtail (Hamachi)</h2></p>
-	<p><h2>Octopus (Tako)</h2></p>
-	<p><h2>White Fish</h2></p>
-	<p><h2>Squid (Ika)</h2></p>
-	<p><h2>Mackeral (Saba)</h2></p>
-	<p><h2>Smoked Salmon</h2></p>
-	
-	<?php
-	} elseif($_GET['type'] == "beverages") {
-	?>
-	<h1>Drinks</h1>
-	<p><h2>Milk</h2></p>
-	<p><h2>Orange Juice</h2></p>
-	<p><h2>Lemonade</h2></p>
-	<p><h2>Iced Tea</h2></p>
-	<p><h2>Green Tea</h2></p>
-	<p><h2>Sodas</h2>
-	<span>Coke, Diet Coke, Sprite, Root Beer.</span></p>
-	
-	<h1>Beer &amp; Wine</h1>
-	<p><h2>Imported Beer</h2>
-	<span>Kirin, Kirin Light, Kirin Lager, Sapporo, Asahi, Heineken.</span></p>
-	<p><h2>Domestic Beer</h2>
-	<span>Bud Light, Coors Light, Michelob, Michelob Light, Miller Light.</span></p>
-	<p><h2>Wine by the Glass</h2>
-	<span>Chardonnay, Chablis, White Zinfandel, Plum Wine, Merlot.</span></p>
-	<p><h2>Sake</h2>
-	<span>Hot, Cold, Premium.</span></p>
-	
-	<?php
-	} else {
-	?>
-	<h1>Raw Special Rolls</h1>
-	<p><h2>Bucs Roll</h2>
-	<span>Crab, cream cheese, asparagus, masago, avocado, tempura crumbs, topped with eel &amp; tuna.</span></p>
-	<p><h2>Candy Cane Roll</h2>
-	<span>Crab delight, cucumber, tempura crumbs, topped with with red and white tuna.</span></p>
-	<p><h2>Centipede Roll</h2>
-	<span>Spicy tuna and avocado topped with tuna tataki, avocado and chili sauce (spicy).</span></p>
-	<p><h2>Christine's Roll</h2>
-	<span>Tuna, salmon, cucumber, onion, topped with scallion, spicy mayo and eel sauce (tempura).</span></p>
-	<p><h2>Matt's Roll</h2>
-	<span>Tuna, shrimp tempura, tempura crumbs, cream cheese, spicy sauce, topped with avocado.</span></p>
-	<p><h2>Pink Lady Roll</h2>
-	<span>Salmon, tuna, masago, avocado wrapped in pink soy paper and scallion.</span></p>
-	<p><h2>Red Samurai Roll</h2>
-	<span>Cucumber, cream cheese, asparagus, crab, avocado, masago, lettuce, topped with tuna (spicy).</span></p>
-	<p><h2>Rising Sun Roll</h2>
-	<span>Salmon, asparagus, cucumber, tempura crumbs, masago, all wrapped in rice paper (spicy).</span></p>
-	<p><h2>Sexy Man Roll</h2>
-	<span>Tuna, avocado, tempura crumbs, eel, masago, served with eel and spicy sauce (tempura).</span></p>
-	<p><h2>Tempura Bagel Roll</h2>
-	<span>Salmon, cream cheese and avocado drizzled with eel sauce (tempura).</span></p>
-	
-	<h1>Cooked Special Rolls</h1>
-	<p><h2>Crispy Roll</h2>
-	<span>Baked salmon, crab, cream cheese, scallions, masago (deep fried with panko).</span></p>
-	<p><h2>Doctor Roll</h2>
-	<span>Tempura shrimp, cream cheese, smoked salmon, asparagus, masago, topped with eel and avocado.</span></p>
-	<p><h2>Gator Roll</h2>
-	<span>California roll with asparagus, topped with gator and eel sauce.</span></p>
-	<p><h2>Last Samurai Roll</h2>
-	<span>Tempura crab stick, asparagus, masago, cucumber, topped with shrimp and avocado.</span></p>
-	<p><h2>Caterpillar Roll</h2>
-	<span>Crab delight, cream cheese, asparagus, topped with avocado.</span></p>
-	<p><h2>Slam Roll</h2>
-	<span>Crab delight, cucumber, tempura crumbs, topped with spicy salmon.</span></p>
-	<p><h2>Waikiki Roll</h2>
-	<span>Tempura shrimp, cream cheese, cucumber, asparagus, masago, topped with mango and avocado with spicy mayo.</span></p>
-	<p><h2>Black &amp; White Roll</h2>
-	<span>Smoked eel, cream cheese, topped with eel sauce (tempura).</span></p>
-	<?php
-	}
-	?>
-</div>
-
-<div id="leftColumnSub">
-	<?php include("includes/menu-nav.php"); ?>
-</div>
-<div class="clearAll"></div>
-
-</div><!-- end copy -->
-
-<div class="clearfooter"></div>
-</div><!-- end container -->
-
-<?php include("includes/footer.php"); ?>
-
-</body>
-
-</html>
-
-
-
-
-
-
-
-
